006ba109b24788183481ad35ace41107b9dd9025
Oleg <olegjan9@rambler.ru>
2022-10-13 07:52:28 +0300

    11.10.2022

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { compose } from 'redux';
+
 import reducer from '../reducers';
 
-const enhancer = (configureStore) => (...args) => {
+const enhancers = (configureStore) => (...args) => {
     const store = configureStore(...args);
-
     const oldDispatch = store.dispatch;
-
     store.dispatch = (action) => {
         if (typeof action === "string") {
             return oldDispatch({type: action})
         }
         return oldDispatch(action )
     }
-
     return store
 }
 
-const store = configureStore({reducer});
+const store = configureStore({reducer, enhancers});
 
 export default store;
