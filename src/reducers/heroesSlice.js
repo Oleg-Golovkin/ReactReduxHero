4dea6c3a66493a618fec46d3264c020ab28cc033
Oleg <olegjan9@rambler.ru>
2022-10-23 20:58:37 +0300

    23.10.2022

--- src/reducers/heroesSlice.js
+++ src/reducers/heroesSlice.js
@@ -25,33 +25,26 @@ const heroesSlice = createSlice({
     name: "heroes",
     initialState,
     reducers: {
-        // heroesFetching: state => {
-        //     state.heroesLoadingStatus = 'HEROES_FETCHING'
-        // },
         heroesFetched: (state, action) => {
             state.heroesLoadingStatus = 'HEROES_FETCHED';
             state.heroes = action.payload
         },
-        // heroesFetchingError: state=> {
-        //     state.heroesLoadingStatus = 'HEROES_FETCHING_ERROR'
-        // },
-        extraReducers: (builder) => {
-            builder
-                .addCase(reduxThunkHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
-                .addCase(reduxThunkHeroes.fulfilled, (state, action) => {
-                    state.heroesLoadingStatus = 'idle';
-                    state.heroes = action.payload;
-                })
-                .addCase(reduxThunkHeroes.rejected, state => {
-                    state.heroesLoadingStatus = 'error';
-                })
-                .addDefaultCase(() => {})
-        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(reduxThunkHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
+            .addCase(reduxThunkHeroes.fulfilled, (state, action) => {
+                state.heroesLoadingStatus = 'idle';
+                state.heroes = action.payload;
+            })
+            .addCase(reduxThunkHeroes.rejected, state => {
+                state.heroesLoadingStatus = 'error';
+            })
+            .addDefaultCase(() => {})
     }
 })
 
 const {reducer, actions} = heroesSlice;
-
 export const {heroesFetched} = actions;
 export default reducer;
 
